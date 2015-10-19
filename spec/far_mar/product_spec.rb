f325ad9bfd9ccddbe5ade0bb5703b37da498bfdc
Jennie Buechner <janina.buechner@gmail.com>
2015-10-19 15:13:34 -0700

    create tests for instantiating all 4 classes

--- spec/far_mar/product_spec.rb
+++ spec/far_mar/product_spec.rb
@@ -1,1 +1,11 @@
-require './spec/spec_helper'
+require 'spec_helper'
+
+describe FarMar do
+  describe FarMar::Product do
+    describe "#initialize" do
+      it "creates a new Product instance" do
+        expect(FarMar::Product.new()).to be_an_instance_of(FarMar::Product)
+      end
+    end
+  end
+end
--- spec/far_mar/sale_spec.rb
+++ spec/far_mar/sale_spec.rb
@@ -1,1 +1,11 @@
-require './spec/spec_helper'
+require 'spec_helper'
+
+describe FarMar do
+  describe FarMar::Sale do
+    describe "#initialize" do
+      it "creates a new Sale instance" do
+        expect(FarMar::Sale.new()).to be_an_instance_of(FarMar::Sale)
+      end
+    end
+  end
+end
