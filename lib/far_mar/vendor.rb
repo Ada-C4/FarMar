ee668fe0fd8d14951a1b6cafcecb9199e15d35ec
Dreedle <davis.audrey.lynn@gmail.com>
2015-10-19 15:58:26 -0700

    Changed order of requires in far_mar.rb

--- lib/far_mar/vendor.rb
+++ lib/far_mar/vendor.rb
@@ -1,6 +1,27 @@
 module FarMar
 
    class Vendor
+
+     def self.all
+     end
+
+     def self.find(id)
+     end
+
+     def self.by_market(market_id)
+
+     def market
+     end
+
+     def products
+     end
+
+     def sales
+     end
+
+     def revenue
+     end
+
    end
-   
+
 end
