81876241a55e064f6f5e308dc4ccfeeb79790fc3
Jennie Buechner <janina.buechner@gmail.com>
2015-10-19 15:40:04 -0700

    create test for Market.all

--- spec/far_mar/market_spec.rb
+++ spec/far_mar/market_spec.rb
@@ -7,5 +7,17 @@ describe FarMar do
         expect(FarMar::Market.new()).to be_an_instance_of(FarMar::Market)
       end
     end
+
+    describe ".all" do
+      before :each do
+        all_markets = FarMar::Market.all
+      end
+      it "returns an array of Market objects" do
+        expect(all_markets).to be_an(Array)
+        expect(all_markets[0]).to be_an_instance_of(FarMar::Market)
+        expect(all_markets[-1]).to be_an_instance_of(FarMar::Market)
+        expect(all_markets.length).to eq(500)
+      end
+    end
   end
 end
