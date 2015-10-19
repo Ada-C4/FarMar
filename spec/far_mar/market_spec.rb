94d096ffcaba5aa81a243e5125d1b250f5de1172
Brittany <BrittanyKohler@gmail.com>
2015-10-19 15:16:01 -0700

    adding tests for initialize methods in spec files

--- spec/far_mar/market_spec.rb
+++ spec/far_mar/market_spec.rb
@@ -1,1 +1,11 @@
 require "./spec/spec_helper"
+
+describe "initialize" do
+  before :each do
+    @market = FarMar::Market.new
+  end
+
+  it "creates an instance of Farmar::Market class" do
+    expect(@market).to be_an_instance_of FarMar::Market
+  end
+end
