e5d6a5f52c0b129e09df342e442b91a0e23ae585
Jenna Nichols <jennaplusplus@gmail.com>
2015-10-19 15:22:22 -0700

    updated spec files

--- spec/far_mar/product_spec.rb
+++ spec/far_mar/product_spec.rb
@@ -1,1 +1,16 @@
 require "spec_helper"
+describe FarMar do
+  describe FarMar::Product do
+
+    before :each do
+      @product = FarMar::Product.new()
+    end
+
+    describe ".new" do
+      it "creates a new instance of the Product class" do
+        expect(@product).to be_an_instance_of(FarMar::Product)
+      end
+    end
+
+  end
+end
--- spec/far_mar/vendor_spec.rb
+++ spec/far_mar/vendor_spec.rb
@@ -1,1 +1,16 @@
 require "spec_helper"
+describe FarMar do
+  describe FarMar::Vendor do
+
+    before :each do
+      @vendor = FarMar::Vendor.new()
+    end
+
+    describe ".new" do
+      it "creates a new instance of the Vendor class" do
+        expect(@vendor).to be_an_instance_of(FarMar::Vendor)
+      end
+    end
+
+  end
+end
