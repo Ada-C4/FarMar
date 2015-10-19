dc5c1b2ba336cc1117dd5167b4940a0f23b8784f
Brittany <BrittanyKohler@gmail.com>
2015-10-19 16:19:50 -0700

    adding #self.all and #self.find methods and tests for market

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -1,5 +1,26 @@
 module FarMar
   class Market
+    attr_reader :id, :name, :address, :city, :county, :state, :zip
 
+    def initialize(id, name, address, city, county, state, zip)
+      @id = id
+      @name = name
+      @address = address
+      @city = city
+      @county = county
+      @state = state
+      @zip = zip
+    end
+
+    def self.all
+      CSV.read("support/markets.csv").map do |line|
+        FarMar::Market.new(line[0].to_i, line[1], line[2], line[3], line[4], line[5], line[6])
+      end
+    end
+
+    def self.find(id)
+      match = CSV.open("support/markets.csv").find { |n| n[0].to_i == id}
+      return FarMar::Market.new(match[0].to_i, match[1], match[2], match[3], match[4], match[5], match[6])
+    end
   end
 end
--- spec/far_mar/market_spec.rb
+++ spec/far_mar/market_spec.rb
@@ -1,11 +1,35 @@
 require "./spec/spec_helper"
 
-describe "initialize" do
-  before :each do
-    @market = FarMar::Market.new
-  end
 
-  it "creates an instance of Farmar::Market class" do
-    expect(@market).to be_an_instance_of FarMar::Market
+describe FarMar::Market do
+  describe "initialize" do
+    before :each do
+      @market = FarMar::Market.new(1, "People's Co-op Farmers Market", "30th and Burnside", "Portland", "Multnomah", "Oregon", "97202")
+    end
+
+    it "creates an instance of Farmar::Market class" do
+      expect(@market).to be_an_instance_of FarMar::Market
+    end
+
+    describe "#self.all" do
+      it "returns an Array" do
+        expect(FarMar::Market.all).to be_an_instance_of Array
+      end
+      it "returns all 500 instances in csv file" do
+        expect(FarMar::Market.all.length).to eq 500
+      end
+      it "returns instances of FarMar::Market" do
+        expect(FarMar::Market.all[0].id).to eq 1
+        expect(FarMar::Market.all[0].name).to eq "People's Co-op Farmers Market"
+        expect(FarMar::Market.all[0].address).to eq "30th and Burnside"
+      end
+    end
+
+    describe "#self.find" do
+      it "returns the FarMar::Market with the specified id" do
+        expect(FarMar::Market.find(2)).to be_an_instance_of FarMar::Market
+        expect(FarMar::Market.find(2).name).to eq "Silverdale Farmers Market"
+      end
+    end
   end
 end
