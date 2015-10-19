5f3f19ddc72f19167725af4668f12bb31c3507e2
Ricky Hougland <houglande@gmail.com>
2015-10-19 14:49:19 -0700

    Created necessary files and their specs in correct folders

---
+++ lib/far_mar.rb
@@ -0,0 +1,8 @@
+require "pry"
+require "time"
+require "rspec"
+require "csv"
+require "./lib/far_mar/market"
+require "./lib/far_mar/vendor"
+require "./lib/far_mar/product"
+require "./lib/far_mar/sale"
