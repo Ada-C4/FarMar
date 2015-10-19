b0544c3b84e7ae707e9665197e014b6ac795ed31
Brittany <BrittanyKohler@gmail.com>
2015-10-19 16:31:54 -0700

    adding #self.all and #self.id methods and tests for Product class

--- lib/far_mar/product.rb
+++ lib/far_mar/product.rb
@@ -1,5 +1,22 @@
 module FarMar
   class Product
+    attr_reader :id, :name, :vendor_id
 
+    def initialize(id, name, vendor_id)
+      @id = id
+      @name = name
+      @vendor_id = vendor_id
+    end
+
+    def self.all
+      CSV.read("support/products.csv").map do |line|
+        FarMar::Product.new(line[0].to_i, line[1], line[2].to_i)
+      end
+    end
+
+    def self.find(id)
+      match = CSV.open("support/products.csv").find { |n| n[0].to_i == id}
+      return FarMar::Product.new(match[0].to_i, match[1], match[2].to_i)
+    end
   end
 end
--- spec/far_mar/product_spec.rb
+++ spec/far_mar/product_spec.rb
@@ -2,10 +2,32 @@ require "./spec/spec_helper"
 
 describe "initialize" do
   before :each do
-    @product = FarMar::Product.new
+    @product = FarMar::Product.new(1, "Dry Beets", 1)
   end
 
   it "creates an instance of Farmar::Product class" do
     expect(@product).to be_an_instance_of FarMar::Product
   end
+
+  describe "#self.all" do
+    it "returns an Array" do
+      expect(FarMar::Product.all).to be_an_instance_of Array
+    end
+    it "returns all 8193 instances in csv file" do
+      expect(FarMar::Product.all.length).to eq 8193
+    end
+    it "returns instances of FarMar::Product" do
+      expect(FarMar::Product.all[0].id).to eq 1
+      expect(FarMar::Product.all[0].name).to eq "Dry Beets"
+      expect(FarMar::Product.all[0].vendor_id).to eq 1
+    end
+  end
+
+  describe "#self.find" do
+    it "returns the FarMar::Product with the specified id" do
+      expect(FarMar::Product.find(2)).to be_an_instance_of FarMar::Product
+      expect(FarMar::Product.find(2).name).to eq "Fierce Greens"
+    end
+  end
+
 end
