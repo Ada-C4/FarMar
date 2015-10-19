36361d9d31b15b71003b4cd6cdb15602942b6317
Kelly <kelly_devlin@alumni.brown.edu>
2015-10-19 16:27:19 -0700

    added #all method to Market class

--- lib/far_mar/Market.rb
+++ lib/far_mar/Market.rb
@@ -1,6 +1,27 @@
 require './lib/far_mar'
 module FarMar
   class Market
-
+    attr_reader :id, :name, :address, :city, :county, :state, :zip
+    def initialize(id, name, address, city, county, state, zip)
+      #fixnum; unique
+      @id = id.to_i
+      #string
+      @name = name
+      #string
+      @address = address
+      #string
+      @city = city
+      #string
+      @county = county
+      #string
+      @state = state
+      #string
+      @zip = zip
+    end
+    def self.all
+      CSV.read("./support/markets.csv").map do |row|
+        FarMar::Market.new(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+      end
+    end
   end
 end
--- spec/far_mar/Market_spec.rb
+++ spec/far_mar/Market_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 describe FarMar::Market do
   before :each do
-    @market = FarMar::Market.new
+    @market = FarMar::Market.new("1", "People's Co-op Farmers Market", "30th and Burnside", "Portland", "Multnomah", "Oregon", "97202")
   end
   describe "#new" do
     it "creates a new Market instance" do
       expect(@market).to be_an_instance_of FarMar::Market
+    end
+  end
+  describe "#all" do
+    it "creates new Market instances" do
+      expect(FarMar::Market.all[0].id).to eq 1
+    end
+    it "returns a collection representing all the markets in the CSV" do
+      expect(FarMar::Market.all.length).to eq 500
     end
   end
 end
