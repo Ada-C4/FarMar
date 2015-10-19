8a8011582960961b2a6e947b954b7b2e81080f8d
Riley Spicer <riley.r.spicer@gmail.com>
2015-10-19 16:22:29 -0700

    Writing specs and code for Market self.all

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -3,6 +3,7 @@
 module FarMar
 
   class Market
+    attr_reader :id, :name, :address, :city, :county, :state, :zip
 
   	def initialize(id, name, address, city, county, state, zip)
   		@id = id
@@ -15,7 +16,12 @@ module FarMar
   	end
 
   	def self.all
-  		CSV.read('./support/markets.csv')
+      market_array = []
+  		CSV.read('./support/markets.csv').each do |row|
+        market = FarMar::Market.new(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+        market_array.push(market)
+      end
+      return market_array
   	end
 
   end
--- spec/far_mar/market_spec.rb
+++ spec/far_mar/market_spec.rb
@@ -1,15 +1,34 @@
 require 'spec_helper'
 
 describe FarMar::Market do 
-	before :each do
-		@market = FarMar::Market.new(1,"People's Co-op Farmers Market","30th and Burnside","Portland","Multnomah","Oregon",97202)
+
+	describe 'initialize' do
+	  before :each do
+		  @market = FarMar::Market.new(1,"People's Co-op Farmers Market","30th and Burnside","Portland","Multnomah","Oregon",97202)
+	  end
+
+	  it 'can create an instance of a FarMar::Market' do
+		  expect(@market).to be_an_instance_of(FarMar::Market)
+	  end
+	  it 'can retrieve the name of the market instance' do
+		  expect(@market.name).to eq ("People's Co-op Farmers Market")
+	  end
+	  it 'assigns the proper variable to state' do
+	  	expect(@market.state).to eq("Oregon")
+	  end
 	end
 
-	it 'can create an instance of a FarMar::Market' do
-		expect(@market).to be_an_instance_of(FarMar::Market)
+	describe 'self.all' do	
+		before :each do
+			@market_array = FarMar::Market.all
+		end
+		it 'returns an array of Market instances' do
+			expect(@market_array).to be_an_instance_of(Array)
+		end
+		it 'contains all the markets in the CSV file' do
+
+		end
 	end
-	it 'can retrieve the name of the market instance' do
-		expect(@market.name).to eq ("People's Co-op Farmers Market")
-	end
+
 
 end
