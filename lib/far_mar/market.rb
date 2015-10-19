96aca23e3f1e8e93c8f173db3e79f43255055b32
Jennie Buechner <janina.buechner@gmail.com>
2015-10-19 16:12:24 -0700

    complete market.all method

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -1,5 +1,42 @@
 module FarMar
   class Market
+    attr_reader :id, :name, :address, :city, :county, :state, :zip
 
+    def initialize(market_hash)
+      @id = market_hash[:id]
+      @name = market_hash[:name]
+      @address = market_hash[:address]
+      @city = market_hash[:city]
+      @county = market_hash[:county]
+      @state = market_hash[:state]
+      @zip = market_hash[:zip]
+    end
+
+    # Converts an array to a hash for passing to owner instantiation
+    def self.convert_to_market_hash(market_array)
+      market_hash = {}
+      market_hash[:id] = market_array[0].to_i
+      market_hash[:name] = market_array[1]
+      market_hash[:address] = market_array[2]
+      market_hash[:city] = market_array[3]
+      market_hash[:county] = market_array[4]
+      market_hash[:state] = market_array[5]
+      return market_hash
+    end
+
+    def self.all
+      csv_file = CSV.read("./support/markets.csv")
+      # Create empty array which will hold all the market objects
+      markets = []
+      csv_file.each do |row|
+        # Convert the array to a hash
+        market_hash = self.convert_to_market_hash(row)
+        # Create a market object from each row-hash in the csv file
+        temp = FarMar::Market.new(market_hash)
+        # Push account object to array of accounts
+        markets.push(temp)
+      end
+      return markets
+    end
   end
 end
--- spec/far_mar/market_spec.rb
+++ spec/far_mar/market_spec.rb
@@ -4,18 +4,30 @@ describe FarMar do
   describe FarMar::Market do
     describe "#initialize" do
       it "creates a new Market instance" do
-        expect(FarMar::Market.new()).to be_an_instance_of(FarMar::Market)
+        fake_hash = {
+          :id => 1,
+          :name => "Holiday",
+          :address => "118 N 180th St",
+          :city => "Shoreline",
+          :county => "King County",
+          :state => "WA",
+          :zip => "98133"
+        }
+        expect(FarMar::Market.new(fake_hash)).to be_an_instance_of(FarMar::Market)
       end
     end
 
     describe ".all" do
-      before :each do
+      # before :each do
+      #
+      # end
+      it "returns an array of 500 Market objects" do
         all_markets = FarMar::Market.all
-      end
-      it "returns an array of Market objects" do
         expect(all_markets).to be_an(Array)
         expect(all_markets[0]).to be_an_instance_of(FarMar::Market)
         expect(all_markets[-1]).to be_an_instance_of(FarMar::Market)
+        expect(all_markets[0].id).to eq(1)
+        expect(all_markets[-1].id).to eq(500)
         expect(all_markets.length).to eq(500)
       end
     end
