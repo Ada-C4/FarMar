8f125de549ec5c8caedfd6892bb9d3f6bc3a23d9
Ricky Hougland <houglande@gmail.com>
2015-10-19 16:25:13 -0700

    created self.all method and specs for vendor

--- lib/far_mar/vendor.rb
+++ lib/far_mar/vendor.rb
@@ -1,8 +1,22 @@
-CSV.read("./support/vendors.csv")
 
 module FarMar
   class Vendor
+    def initialize(id, name, num_employees, market_id)
+      @id = id
+      @name = name
+      @num_employees = num_employees
+      @market_id = market_id
+    end
+
     def self.all
+      all_vendors = []
+
+      CSV.read("./support/vendors.csv").each do |line|
+        y = FarMar::Vendor.new(line[0].to_i, line[1], line[2].to_i, line[3].to_i)
+        all_vendors.push(y)
+      end
+
+      return all_vendors
     end
 
     def self.find(id)
--- spec/far_mar/vendor_spec.rb
+++ spec/far_mar/vendor_spec.rb
@@ -1,12 +1,23 @@
 require "./spec/spec_helper"
 
 describe FarMar::Vendor do
+  before :each do
+    @vendor1 = FarMar::Vendor.new(5, "name", 7, 8)
+    @vendor2 = FarMar::Vendor.new(6, "name", 9, 10)
+  end
   describe "#initialize" do
     it "creates new instance of vendor" do
-      @vendor1 = FarMar::Vendor.new
-      @vendor2 = FarMar::Vendor.new
       expect(@vendor1).to be_instance_of FarMar::Vendor
       expect(@vendor2).to be_instance_of FarMar::Vendor
+    end
+  end
+
+  describe "#self.all" do
+    it "returns an array" do
+      expect(FarMar::Vendor.all).to be_an Array
+    end
+    it "returns 2690 items in array" do
+      expect(FarMar::Vendor.all.length).to eq 2690
     end
   end
 end
