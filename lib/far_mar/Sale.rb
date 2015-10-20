699b2e60f808a7c19bcb600b1ef6e81fe8a2fdf1
Kelly <kelly_devlin@alumni.brown.edu>
2015-10-20 14:47:38 -0700

    Added #all method to Sale Class

--- lib/far_mar/Sale.rb
+++ lib/far_mar/Sale.rb
@@ -1,6 +1,18 @@
 require './lib/far_mar'
 module FarMar
   class Sale
-
+    attr_reader :id, :amount, :purchase_time, :vendor_id, :product_id
+    def initialize(id, amount, purchase_time, vendor_id, product_id)
+      @id = id.to_i
+      @amount = amount.to_i
+      @purchase_time = DateTime.strptime(purchase_time, "%Y-%m-%d %H:%M:%S %z")
+      @vendor_id = vendor_id.to_i
+      @product_id = product_id.to_i
+    end
+    def self.all
+      CSV.read("./support/sales.csv").map do |row|
+        FarMar::Sale.new(row[0], row[1], row[2], row[3], row[4])
+      end
+    end
   end
 end
--- spec/far_mar/Sale_spec.rb
+++ spec/far_mar/Sale_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 describe FarMar::Sale do
   before :each do
-    @sale = FarMar::Sale.new
+    @sale = FarMar::Sale.new("1", "9290", "2013-11-07 04:34:56 -0800", "1", "1")
   end
   describe "#new" do
     it "creates a new Sale instance" do
       expect(@sale).to be_an_instance_of FarMar::Sale
+    end
+  end
+  describe "#all" do
+    it "creates new Sale instances" do
+      expect(FarMar::Sale.all[1].amount).to eq 2262
+    end
+    it "returns a collection representing all sales in the CSV" do
+      expect(FarMar::Sale.all.length).to eq 12798
     end
   end
 end
