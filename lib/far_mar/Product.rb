e35490f850b8dfc30540bbe20b2d8771b1f2b6b2
Kelly <kelly_devlin@alumni.brown.edu>
2015-10-20 14:22:21 -0700

    Added #all method to Product class

--- lib/far_mar/Product.rb
+++ lib/far_mar/Product.rb
@@ -1,6 +1,16 @@
 require './lib/far_mar'
 module FarMar
   class Product
-
+    attr_reader :id, :name, :vendor_id
+    def initialize(id, name, vendor_id)
+      @id = id.to_i
+      @name = name
+      @vendor_id = vendor_id.to_i
+    end
+    def self.all
+      CSV.read("./support/products.csv").map do |row|
+        FarMar::Product.new(row[0], row[1], row[2])
+      end
+    end
   end
 end
--- spec/far_mar/Product_spec.rb
+++ spec/far_mar/Product_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 describe FarMar::Product do
   before :each do
-    @product = FarMar::Product.new
+    @product = FarMar::Product.new("1", "Dry", "Beets")
   end
   describe "#new" do
     it "creates a new Product instance" do
       expect(@product).to be_an_instance_of FarMar::Product
+    end
+  end
+  describe "#all" do
+    it "creates new Product instances" do
+      expect(FarMar::Product.all[0].name).to eq "Dry Beets"
+    end
+    it "returns a collection representing all the products in the CSV" do
+      expect(FarMar::Product.all.length).to eq 8193
     end
   end
 end
