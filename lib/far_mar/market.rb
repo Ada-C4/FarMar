378b73eed816cab1cd000591185cb164b6b6ec92
Amy Hunter <amyrhunter@gmail.com>
2015-10-20 16:37:17 -0700

    Got the self.all method for the Market class to work.

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -3,7 +3,7 @@ require 'csv'
 module FarMar
   class Market
     attr_accessor :id, :name, :address, :city, :county, :state, :zip
-    def initialize()
+    def initialize(id, name, address, city, county, state, zip)
       @id = id
       @name = name
       @address = address
@@ -13,10 +13,14 @@ module FarMar
       @zip = zip
     end
 
-    # def self.all
-    #   #returns a collection of Market instances, representing all of the markets described in the CSV
-    #
-    #   return market_list
-    # end
+    def self.all
+      #returns a collection of Market instances, representing all of the markets described in the CSV
+      market_array = []
+      CSV.read("./support/markets.csv").each do |market|
+        print market
+        list_of_markets = Market.new(market[0], market[1], market[2], market[3], market[4], market[5], market[6])
+        market_array.push(list_of_markets)
+      end
+    end
   end
 end
