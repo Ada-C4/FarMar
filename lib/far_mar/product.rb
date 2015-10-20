aad588a7dbb9e066cbbdd2b7dc82bc1d3c14d128
Jenna Nichols <jennaplusplus@gmail.com>
2015-10-20 16:33:01 -0700

    added self.all method/tests for Product class

--- lib/far_mar/product.rb
+++ lib/far_mar/product.rb
@@ -1,9 +1,20 @@
 module FarMar
   class Product
+    attr_reader :id, :vendor_id
+
+    def initialize(product_info)
+      @id = product_info[0]
+      @name = product_info[1]
+      @vendor_id = product_info[2]
+    end
 
     def self.all
-      products = CSV.read("./support/products.csv")
-      return products
+      product_array = []
+      CSV.read("./support/products.csv").each do |product|
+        new_product = FarMar::Product.new(product)
+        product_array.push(new_product)
+      end
+      return product_array
     end
 
   end
--- spec/far_mar/product_spec.rb
+++ spec/far_mar/product_spec.rb
@@ -2,12 +2,16 @@ require "spec_helper"
 describe FarMar do
   describe FarMar::Product do
 
-    describe ".new" do
+    describe "#initialize" do
       before :each do
-        @product = FarMar::Product.new
+        product_info = [1,"Dry Beets",1]
+        @product = FarMar::Product.new(product_info)
       end
       it "creates a new instance of the Product class" do
         expect(@product).to be_an_instance_of(FarMar::Product)
+      end
+      it "retrieves the product ID from the product info array" do
+        expect(@product.id).to eq(1)
       end
     end
 
@@ -18,8 +22,14 @@ describe FarMar do
       it "returns an array" do
         expect(FarMar::Product.all).to be_an(Array)
       end
-      it "returns an array with the vendor info" do
+      it "returns an array with all the products" do
         expect(FarMar::Product.all.length).to eq(@products.size)
+      end
+      it "contains instances of the Product class" do
+        vendor_total = FarMar::Product.all.length
+        expect(FarMar::Product.all[0]).to be_an_instance_of(FarMar::Product)
+        expect(FarMar::Product.all[vendor_total - 1]).to be_an_instance_of(FarMar::Product)
+        expect(FarMar::Product.all[vendor_total]).to be nil
       end
     end
 
