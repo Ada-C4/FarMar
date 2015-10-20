1f43d3938828df9634613f57750c758b4670be27
Amy Hunter <amyrhunter@gmail.com>
2015-10-20 16:26:31 -0700

    Working initialize methods for Sale and Vendor classes.

--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -1,4 +1,12 @@
 module FarMar
   class Sale
+    attr_accessor :id, :amount, :purchase_time, :vendor_id, :product_id
+    def initialize
+      @id = id
+      @amount = amount
+      @purchase_time = purchase_time
+      @vendor_id = vendor_id
+      @product_id = product_id
+    end
   end
 end
--- lib/far_mar/vendor.rb
+++ lib/far_mar/vendor.rb
@@ -1,4 +1,11 @@
 module FarMar
   class Vendor
+    attr_accessor :id, :name, :no_of_employees, :market_id
+      def initialize
+        @id = id
+        @name = name
+        @no_of_employees = no_of_employees
+        @market_id = market_id
+      end
   end
 end
