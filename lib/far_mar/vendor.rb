f6fc4b39c92ec15df49fe8187f4b283f93e18215
Amy Hunter <amyrhunter@gmail.com>
2015-10-20 16:48:22 -0700

    Working self.all method for Vendor class.

--- lib/far_mar/vendor.rb
+++ lib/far_mar/vendor.rb
@@ -1,11 +1,21 @@
 module FarMar
   class Vendor
     attr_accessor :id, :name, :no_of_employees, :market_id
-      def initialize
+      def initialize(id, name, no_of_employees, market_id)
         @id = id
         @name = name
         @no_of_employees = no_of_employees
         @market_id = market_id
+      end
+
+      def self.all
+        #returns a collection of vendor instances, representing all of the vendors described in the CSV
+        vendor_array = []
+        CSV.read("./support/vendors.csv").each do |vendor|
+          print vendor
+          list_of_vendors = Vendor.new(vendor[0], vendor[1], vendor[2], vendor[3])
+          vendor_array.push(list_of_vendors)
+        end
       end
   end
 end
