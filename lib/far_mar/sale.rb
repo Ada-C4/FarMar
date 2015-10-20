e166fe2562cfc585034c35bc5cd546efb60abb0c
Amy Hunter <amyrhunter@gmail.com>
2015-10-20 16:45:20 -0700

    Working self.all method for the Sale class.

--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -1,12 +1,22 @@
 module FarMar
   class Sale
     attr_accessor :id, :amount, :purchase_time, :vendor_id, :product_id
-    def initialize
+    def initialize(id, amount, purchase_time, vendor_id, product_id)
       @id = id
       @amount = amount
       @purchase_time = purchase_time
       @vendor_id = vendor_id
       @product_id = product_id
+    end
+
+    def self.all
+      #returns a collection of sale instances, representing all of the sales described in the CSV
+      sale_array = []
+      CSV.read("./support/sales.csv").each do |sale|
+        print sale
+        list_of_sales = Sale.new(sale[0], sale[1], sale[2], sale[3], sale[4])
+        sale_array.push(list_of_sales)
+      end
     end
   end
 end
