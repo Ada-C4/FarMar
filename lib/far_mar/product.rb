2ce493b9686f294bf295a8a8ad9d7352b9fe1317
Daphne <dg1181@nyu.edu>
2015-10-19 17:08:41 -0700

    created self.all and self.find(id) and tests for product.rb

--- lib/far_mar/product.rb
+++ lib/far_mar/product.rb
@@ -1,4 +1,31 @@
 module FarMar
   class Product
+    attr_accessor :id, :name, :vendor_id
+
+    def initialize(id, name, vendor_id)
+      @id = id.to_i
+      @name = name
+      @vendor_id = vendor_id.to_i
+    end
+
+    def self.all
+      products_list = []
+      products_csv = CSV.read("./support/products.csv")
+
+      products_csv.each do |row|
+        product = FarMar::Product.new(row[0], row[1], row[2])
+        products_list.push(product)
+      end
+
+      return products_list
+    end
+
+    def self.find(id)
+      products_list = self.all
+
+      products_list.find do |instance|
+        instance.id == id
+      end
+    end
   end
 end
--- spec/far_mar/product_spec.rb
+++ spec/far_mar/product_spec.rb
@@ -2,11 +2,34 @@ require "spec_helper"
 
 describe FarMar::Product do
   before :each do
-    @product = FarMar::Product.new
+    row = [1, "Product", 2]
+    @product = FarMar::Product.new(row[0], row[1], row[2])
   end
   describe ".new" do
     it "creates a new instance of Product" do
       expect(@product).to be_an_instance_of FarMar::Product
+    end
+  end
+  describe "#initialize" do
+    it "creates a new instance of product with 3 instance variables" do
+      expect(@product.instance_variables.length).to eq 3
+    end
+    it "creates a new instance of product with @vendor_id as an integer" do
+      expect(@product.vendor_id.class).to eq Fixnum
+    end
+  end
+  describe "self.all" do
+    it "returns an array of product instances" do
+      expect(FarMar::Product.all).to be_an Array
+    end
+    it "returns an array with 8193 product instances" do
+      expect(FarMar::Product.all.length).to eq 8193
+    end
+  end
+  describe "self.find(id)" do
+    it "returns the corresponding product name for a particular product id" do
+      id = 8171
+      expect(FarMar::Product.find(id).name).to eq "Lazy Pretzel"
     end
   end
 end
