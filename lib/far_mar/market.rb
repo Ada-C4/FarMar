978e4578cddd0bbec7deee8b2d4a2e1741bc9b71
Dreedle <davis.audrey.lynn@gmail.com>
2015-10-20 14:24:33 -0700

    Fixed vendor bug that had been in baseline. Rspecs run now

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -4,31 +4,29 @@ module FarMar
      attr_reader :ID, :name, :address, :city, :county, :state, :zip
      #the choice- to iterate or to read and manipulate?
      #what datatype do I ultimately want? Probably a hash.
-     CSV.open(filename, mode='r', options) {|file| block}
-
-    CSV.open("moar_ada_peeps.csv", 'w') do |csv|
-      csv << "5,Charles"
-    end
-
-
-    CSV.open("markets.csv", 'r').each do |line|
-      puts line
-    end
+    #  CSV.open(filename, mode='r', options) {|file| block}
+    #
+    # CSV.open("moar_ada_peeps.csv", 'w') do |csv|
+    #   csv << "5,Charles"
+    # end
+    #
+    #
+    # CSV.open("markets.csv", 'r').each do |line|
+    #   puts line
+    # end
 
 
      def initialize
-
-
      end
 
 
 
      def self.all
-       returns market_instances_array, which should be an array of objects
+      #  returns market_instances_array, which should be an array of objects
      end
 
      def self.find(id)
-       returns instance where the value matches the parameter
+      #  returns instance where the value matches the parameter
      end
 
      def vendors
--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -3,8 +3,8 @@ module FarMar
    class Sale
      attr_reader :ID, :amount, :purchase_time, :vendor_id, :product_id
 
-     @open_date = DateTime.strptime(open_date, "%Y-%m-%d %H:%M:%S %z")
-        end
+    #  @open_date = DateTime.strptime(open_date, "%Y-%m-%d %H:%M:%S %z")
+    #     end
 
      def self.all
      end
