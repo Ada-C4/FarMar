26c773311e876ab309ddcd96a509bac1da5f8526
Jennie Buechner <janina.buechner@gmail.com>
2015-10-20 14:28:50 -0700

    create vendor initialize test

--- spec/far_mar/vendor_spec.rb
+++ spec/far_mar/vendor_spec.rb
@@ -4,7 +4,14 @@ describe FarMar do
   describe FarMar::Vendor do
     describe "#initialize" do
       it "creates a new Vendor instance" do
-        expect(FarMar::Vendor.new()).to be_an_instance_of(FarMar::Vendor)
+        v_array = %w(1 QFC 10 2)
+        v_hash = FarMar::Vendor.convert_to_hash(v_array)
+        vendor = FarMar::Vendor.new(v_hash)
+        expect(vendor).to be_an_instance_of(FarMar::Vendor)
+        expect(vendor.id).to eq(1)
+        expect(vendor.name).to eq("QFC")
+        expect(vendor.emp_num).to eq(10)
+        expect(vendor.market_id).to eq(2)
       end
     end
   end
