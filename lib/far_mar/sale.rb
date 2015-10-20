9a6162c04d0bac60005b0d355790d7922a6d6919
Jenna Nichols <jennaplusplus@gmail.com>
2015-10-20 16:38:55 -0700

    updated vendor with self.all

--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -1,10 +1,22 @@
 module FarMar
   class Sale
+    attr_reader :id, :vendor_id, :product_id
 
-    def self.all
-      sales = CSV.read("./support/sales.csv")
-      return sales
+    def initialize(sale_info)
+      @id = sale_info[0]
+      @amount = sale_info[1]
+      @purchase_time = sale_info[2]
+      @vendor_id = sale_info[3]
+      @product_id = sale_info[4]
     end
 
+    def self.all
+      sale_array = []
+      CSV.read("./support/sales.csv").each do |sale|
+        new_sale = FarMar::Sale.new(sale)
+        sale_array.push(new_sale)
+      end
+      return sale_array
+    end
   end
 end
--- spec/far_mar/sale_spec.rb
+++ spec/far_mar/sale_spec.rb
@@ -2,12 +2,16 @@ require "spec_helper"
 describe FarMar do
   describe FarMar::Sale do
 
-    describe ".new" do
+    describe "#initialize" do
       before :each do
-        @sale = FarMar::Sale.new
+        sale_info = [1,9290,"2013-11-07 04:34:56 -0800",1,1]
+        @sale = FarMar::Sale.new(sale_info)
       end
       it "creates a new instance of the Sale class" do
         expect(@sale).to be_an_instance_of(FarMar::Sale)
+      end
+      it "retrieves the sale ID from the sale info array" do
+        expect(@sale.id).to eq(1)
       end
     end
 
@@ -18,8 +22,14 @@ describe FarMar do
       it "returns an array" do
         expect(FarMar::Sale.all).to be_an(Array)
       end
-      it "returns an array with the vendor info" do
+      it "returns an array with all the sales" do
         expect(FarMar::Sale.all.length).to eq(@sales.size)
+      end
+      it "contains instances of the Sale class" do
+        vendor_total = FarMar::Sale.all.length
+        expect(FarMar::Sale.all[0]).to be_an_instance_of(FarMar::Sale)
+        expect(FarMar::Sale.all[vendor_total - 1]).to be_an_instance_of(FarMar::Sale)
+        expect(FarMar::Sale.all[vendor_total]).to be nil
       end
     end
 
