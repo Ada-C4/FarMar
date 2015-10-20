c3e1f210dc7db39c70f4860904eb5cbdcedd1da3
Jennie Buechner <janina.buechner@gmail.com>
2015-10-20 14:32:09 -0700

    create vendor initialize and convert_to_hash methods

--- lib/far_mar/vendor.rb
+++ lib/far_mar/vendor.rb
@@ -1,5 +1,18 @@
 module FarMar
-  class Vendor
+  class Vendor < FarMar_Base
+    attr_reader :id, :name, :emp_num, :market_id
 
+    def initialize(vendor_hash)
+      super(vendor_hash)
+    end
+
+    def self.convert_to_hash(array)
+      vendor = {}
+      vendor[:id] = array[0].to_i
+      vendor[:name] = array[1]
+      vendor[:emp_num] = array[2].to_i
+      vendor[:market_id] = array[3].to_i
+      return vendor
+    end
   end
 end
