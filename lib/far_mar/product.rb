390737e71de0d4683c6e9534361c26e3b4ebb9d2
Riley Spicer <riley.r.spicer@gmail.com>
2015-10-20 16:03:22 -0700

    Product #initialize and .self tests passing.

--- lib/far_mar/product.rb
+++ lib/far_mar/product.rb
@@ -3,9 +3,21 @@
 module FarMar
 
 	class Product
+		attr_reader :product_id, :name, :vendor_id
 
-		def initialize()
-			
+		def initialize(product_id, name, vendor_id)
+			@product_id = product_id
+			@name = name.to_s
+			@vendor_id = vendor_id
+		end
+
+		def self.all
+			product_array = []
+			CSV.read('./support/products.csv').each do |row|
+        product = FarMar::Product.new(row[0], row[1], row[2])
+        product_array.push(product)
+			end
+			return product_array
 		end
 
 	end
