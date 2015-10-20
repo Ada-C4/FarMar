7e9b73eda633b8432c0402999411412a9c5881d5
Jenna Nichols <jennaplusplus@gmail.com>
2015-10-20 16:23:03 -0700

    updated initialize method/tests for Market

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -1,9 +1,25 @@
 module FarMar
   class Market
+    attr_reader :id
+
+    def initialize(market_info)
+      @id = market_info[0]
+      @name = market_info[1]
+      @address = market_info[2]
+      @city = market_info[3]
+      @county = market_info[4]
+      @state = market_info[5]
+      @zip = market_info[6]
+    end
+
 
     def self.all
-      markets = CSV.read("./support/markets.csv")
-      return markets
+      market_array = []
+      CSV.read("./support/markets.csv").each do |market|
+        new_market = FarMar::Market.new(market)
+        market_array.push(new_market)
+      end
+      return market_array
     end
 
   end
--- spec/far_mar/market_spec.rb
+++ spec/far_mar/market_spec.rb
@@ -2,12 +2,16 @@ require "spec_helper"
 describe FarMar do
   describe FarMar::Market do
 
-    describe ".new" do
+    describe "#initialize" do
       before :each do
-        @market = FarMar::Market.new
+        market_info = [1,"People's Co-op Farmers Market","30th and Burnside","Portland","Multnomah","Oregon","97202"]
+        @market = FarMar::Market.new(market_info)
       end
       it "creates a new instance of the Market class" do
         expect(@market).to be_an_instance_of(FarMar::Market)
+      end
+      it "retrieves the market ID from the market info array" do
+        expect(@market.id).to eq(1)
       end
     end
 
@@ -18,8 +22,14 @@ describe FarMar do
       it "returns an array" do
         expect(FarMar::Market.all).to be_an(Array)
       end
-      it "returns an array with the vendor info" do
+      it "returns an array with all the markets" do
         expect(FarMar::Market.all.length).to eq(@markets.size)
+      end
+      it "contains instances of the Market class" do
+        market_total = FarMar::Market.all.length
+        expect(FarMar::Market.all[0]).to be_an_instance_of(FarMar::Market)
+        expect(FarMar::Market.all[market_total - 1]).to be_an_instance_of(FarMar::Market)
+        expect(FarMar::Market.all[market_total]).to be nil
       end
     end
 
