2c4ad5de852db759c5d04f4afcf6424a5cc7b1f4
Lauren Granger <lr.granger21@gmail.com>
2015-10-20 13:20:53 -0700

    spec and method for self.all in Market class

--- lib/far_mar/market_class.rb
+++ lib/far_mar/market_class.rb
@@ -1,7 +1,52 @@
 module FarMar
   class Market
+
+    attr_accessor :market_array, :all_markets_array
+
+    @@num_of_markets = 0
+
+  # Each individual market has many vendors associated with it. The `FarMar::Market` data, in order in the CSV, consists of:
+  #
+  # 1. ID - (Fixnum) a unique identifier for that market
+  # 2. Name - (String) the name of the market (not guaranteed unique)
+  # 3. Address - (String) street address of the market
+  # 4. City - (String) city in which the market is located
+  # 5. County - (String) county in which the market is located
+  # 6. State - (String) state in which the market is located
+  # 7. Zip - (String) zipcode in which the market is located
+
     def initialize
-      market_array = CSV.read("./support/markets.csv")
+      @market_array = CSV.read("./support/markets.csv")
+      @id = @market_array[@@num_of_markets][0]
+      @name = @market_array[@@num_of_markets][1]
+      @address = @market_array[@@num_of_markets][2]
+      @city = @market_array[@@num_of_markets][3]
+      @county = @market_array[@@num_of_markets][4]
+      @state = @market_array[@@num_of_markets][5]
+      @zip = @market_array[@@num_of_markets][6]
+      @@num_of_markets += 1
     end
+
+# - `self.all` - returns a collection of Market instances, representing all of the markets described in the CSV
+    def self.all
+      market_array = CSV.read("./support/markets.csv")
+      @all_markets_array = []
+      @@num_of_markets = 0
+      while @@num_of_markets < (market_array.length) do
+        @all_markets_array.push(FarMar::Market.new)
+      end
+      return @all_markets_array
+    end
+#
+# # - `self.find(id)` - returns an instance of Market where the value of the `id` field in the CSV matches the passed parameter.
+#
+#     def self.find(id)
+#       @all_markets_array.find do |x|
+#         x = id
+#       end
+#     end
+
+# - `vendors` - returns a collection of `FarMar::Vendor` instances that are associated with the market by the `market_id` field.
+
   end
 end
--- spec/far_mar/markets_spec.rb
+++ spec/far_mar/markets_spec.rb
@@ -2,6 +2,7 @@ require "./spec/spec_helper.rb"
 require "./lib/far_mar/market_class.rb"
 
 describe FarMar::Market do
+
   describe "initialize"
   before :each do
     @market_test = FarMar::Market.new
@@ -9,4 +10,12 @@ describe FarMar::Market do
     it "creates a new instace of the market class" do
       expect(@market_test).to be_an_instance_of FarMar::Market
     end
+    it "converts the csv file to an array" do
+      expect(@market_test.market_array).to be_an Array
+    end
+
+    describe "self.all"
+      it "returns an array of all markets in the csv file" do
+        expect(FarMar::Market.all.length).to eq 500
+      end
 end
