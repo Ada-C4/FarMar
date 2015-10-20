e3790e8f390471bec1cf9f0eb9577dcb37333f29
Amy Hunter <amyrhunter@gmail.com>
2015-10-20 16:41:09 -0700

    Working self.all method for the Product class.

--- lib/far_mar/product.rb
+++ lib/far_mar/product.rb
@@ -1,10 +1,20 @@
 module FarMar
   class Product
     attr_accessor :id, :name, :vendor_id
-    def initialize()
+    def initialize(id, name, vendor_id)
       @id = id
       @name = name
       @vendor_id = vendor_id
+    end
+
+    def self.all
+      #returns a collection of product instances, representing all of the products described in the CSV
+      product_array = []
+      CSV.read("./support/products.csv").each do |product|
+        print product
+        list_of_products = Product.new(product[0], product[1], product[2])
+        product_array.push(list_of_products)
+      end
     end
   end
 end
