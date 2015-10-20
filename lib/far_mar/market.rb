8afacac3682a349c189bfad479a0b67e4286871c
Wesley Willis <haileyw@gmail.com>
2015-10-19 18:41:57 -0700

    add self.new and spec for market class

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -1,7 +1,39 @@
+require "pry"
 module FarMar
-  class Market
-    def initialize
 
+  class Market
+    attr_accessor :id, :name, :address, :city, :county, :state, :zip
+    def initialize(market_hash)
+      @id = market_hash[:id]
+      @name = market_hash[:name]
+      @address = market_hash[:address]
+      @city = market_hash[:city]
+      @county = market_hash[:county]
+      @state = market_hash[:state]
+      @zip = market_hash[:zip]
     end
+
+    def self.all
+      markets_all = []
+      markets_data = CSV.read("support/markets.csv")
+
+      markets_data.each do |id, name, address, city, county, state, zip|
+        #binding.pry
+        market_hash = {:id => id, :name => name, :address => address, :city => city, :county => county, :state => state, :zip => zip}
+        market = FarMar::Market.new(market_hash)
+        markets_all.push(market)
+      end
+      return markets_all
+    end
+
+    #def self.all
+    #  markets_csv = CSV.read("./support/markets")
+    #  markets_csv.foreach do |id, name, address, city, county, state, zip|
+    #    Market.new(:id => id, :name => name, :address => address, :city => city, :county => county, :state => state, :zip => zip)
+    #  end
+    #end
   end
 end
+    #CSV.foreach(file_path, :headers => true, :col_sep => ';') do |row|
+  #Car.create(:make => row[0], :model => row[1], :year => row[2])
+#end
--- spec/far_mar/market_spec.rb
+++ spec/far_mar/market_spec.rb
@@ -3,13 +3,23 @@ require "spec_helper"
 describe FarMar do
   describe FarMar::Market do
     before :each do
-      @market = FarMar::Market.new
+      @market = FarMar::Market.all
     end
 
     describe "#initialize" do
       it "creates a new instance of Market" do
-        expect(@market).to be_an_instance_of FarMar::Market
+        fake_hash = {:id => 666, :name => "Boo Boo", :address => "123 Haunted House Way", :city => "Spooky Estates", :county => "Cook", :state => "IL", :zip => 60616}
+        test = FarMar::Market.new(fake_hash)
+        expect(test).to be_an_instance_of FarMar::Market
       end
     end
+
+    describe '#self.all' do
+      it "returns a collection of Market instances" do
+        
+        expect(@market.length).to eq 4
+      end
+
+    end
   end
-end     
+end
