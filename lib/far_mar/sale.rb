e4d3f7aa8cbb05356fd6aadd95f7d21a7c08edaa
Brittany <BrittanyKohler@gmail.com>
2015-10-19 22:31:40 -0700

    adding initialize, #self.all, and #self.find methods and tests for Sale Class. Still one test failing, related to DateTime

--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -1,5 +1,24 @@
 module FarMar
   class Sale
+    attr_reader :id, :amount, :purchase_time, :vendor_id, :product_id
 
+    def initialize(id, amount, purchase_time, vendor_id, product_id)
+      @id = id
+      @amount = amount
+      @purchase_time = DateTime.strptime(purchase_time, "%Y-%m-%d %H:%M:%S %z")
+      @vendor_id = vendor_id
+      @product_id = product_id
+    end
+
+    def self.all
+      CSV.read("support/sales.csv").map do |line|
+        FarMar::Sale.new(line[0].to_i, line[1].to_i, line[2], line[3].to_i, line[4].to_i)
+      end
+    end
+
+    def self.find(id)
+      match = CSV.open("support/markets.csv").find { |n| n[0].to_i == id}
+      return FarMar::Sale.new(match[0].to_i, match[1].to_i, match[2].to_s, match[3].to_i, match[4].to_i)
+    end
   end
 end
--- spec/far_mar/sale_spec.rb
+++ spec/far_mar/sale_spec.rb
@@ -1,11 +1,34 @@
 require "./spec/spec_helper"
 
-describe "initialize" do
-  before :each do
-    @sale = FarMar::Sale.new
+describe FarMar::Sale do
+  describe "initialize" do
+    before :each do
+      @sale = FarMar::Sale.new(1, 9290, "2013-11-07 04:34:56 -0800", 1, 1)
+    end
+
+    it "creates an instance of Farmar::Sale class" do
+      expect(@sale).to be_an_instance_of FarMar::Sale
+    end
   end
 
-  it "creates an instance of Farmar::Sale class" do
-    expect(@sale).to be_an_instance_of FarMar::Sale
+  describe "#self.all" do
+    it "returns an Array" do
+      expect(FarMar::Sale.all).to be_an_instance_of Array
+    end
+    it "returns all 12001 instances in csv file" do
+      expect(FarMar::Sale.all.length).to eq 12798
+    end
+    it "returns instances of FarMar::Sale" do
+      expect(FarMar::Sale.all[0].id).to eq 1
+      expect(FarMar::Sale.all[0].amount).to eq 9290
+      expect(FarMar::Sale.all[0].vendor_id).to eq 1
+    end
+  end
+
+  describe "#self.find" do
+    it "returns the FarMar::Sale with the specified id" do
+      expect(FarMar::Sale.find(2)).to be_an_instance_of FarMar::Sale
+      expect(FarMar::Sale.find(2).amount).to eq 2262
+    end
   end
 end
