817c020cb2de95147f64d13b392ccfbd72c98d25
Kelly <kelly_devlin@alumni.brown.edu>
2015-10-20 15:02:08 -0700

    added #all method to Vendor class

--- lib/far_mar/Vendor.rb
+++ lib/far_mar/Vendor.rb
@@ -1,6 +1,17 @@
 require './lib/far_mar'
 module FarMar
   class Vendor
-
+    attr_reader :id, :name, :num_employees, :market_id
+    def initialize(id, name, num_employees, market_id)
+      @id = id.to_i
+      @name = name
+      @num_employees = num_employees.to_i
+      @market_id = market_id.to_i
+    end
+    def self.all
+      CSV.read("./support/vendors.csv").map do |row|
+        FarMar::Vendor.new(row[0], row[1], row[2], row[3])
+      end
+    end
   end
 end
--- spec/far_mar/Vendor_spec.rb
+++ spec/far_mar/Vendor_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 describe FarMar::Vendor do
   before :each do
-    @vendor = FarMar::Vendor.new
+    @vendor = FarMar::Vendor.new("6", "Zulauf and Sons", "8", "1")
   end
   describe "#new" do
     it "creates a new Vendor instance" do
       expect(@vendor).to be_an_instance_of FarMar::Vendor
+    end
+  end
+  describe "#all" do
+    it "creates a new Vendor instance" do
+      expect(FarMar::Vendor.all[3].market_id).to eq 1
+    end
+    it "returns a collection representing all vendors in CSV" do
+      expect(FarMar::Vendor.all.length).to eq 2690
     end
   end
 end
