bb9902f68b085a271b51517e31cb9dfffacf0164
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-10-21 11:06:48 -0700

    Added market all and find methods and specs for those methods

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -1,4 +1,60 @@
+require "csv"
+require "pry"
+
 module FarMar
+  attr_accessor :id, :name, :address, :city, :county, :state, :zip
   class Market
+    def initialize(market_hash)
+      @id = market_hash[:id]
+      @name = market_hash[:name]
+      @address = market_hash[:address]
+      @city = market_hash[:city]
+      @county = market_hash[:county]
+      @state = market_hash[:state]
+      @zip = market_hash[:zip]
+    end
+
+    def self.all()
+      markets_csv = CSV.read("./support/markets.csv")
+      markets_array = []
+      markets_csv.each do |line|
+      new_market = FarMar::Market.new(
+      {
+        :id => line[0].to_i,
+        :name => line[1],
+        :address => line[2],
+        :city => line[3],
+        :county => line[4],
+        :state => line[5],
+        :zip => line[6]
+        }
+      )
+      markets_array.push(new_market)
+    end
+    return markets_array
+    end
+
+    def self.find(id)
+      markets_csv = CSV.read("./support/markets.csv")
+      match = markets_csv.find {|market| market[0].to_i == id}
+      new_market = FarMar::Market.new(
+      {
+        :id => match[0].to_i,
+        :name => match[1],
+        :address => match[2],
+        :city => match[3],
+        :county => match[4],
+        :state => match[5],
+        :zip => match[6]
+        }
+      )
+      return new_market
+    end
+
+
+    def id
+      return @id
+    end
+
   end
 end
--- spec/far_mar/market_spec.rb
+++ spec/far_mar/market_spec.rb
@@ -2,9 +2,61 @@ require "spec_helper"
 
 describe FarMar do
   describe FarMar::Market do
+    before :each do
+    end
+
     describe "initialize" do
       it "creates an instance of a Market" do
-        expect(FarMar::Market.new).to be_an_instance_of FarMar::Market
+        @market_hash = {
+        :id => 123,
+        :name => "market name",
+        :address => "market address",
+        :city => "market city",
+        :county => "market county",
+        :state => "market state",
+        :zip => 12345
+        }
+        expect(FarMar::Market.new(@market_hash)).to be_an_instance_of FarMar::Market
+      end
+      # it "returns an error if not passed a market_hash" do
+      #   @market_array = ["a","b"]
+      #   expect(FarMar::Market.new(@market_array)).to raise_error TypeError, "no implicit conversion of Symbol into Integer"
+      # end
+    end
+
+    describe "self.all" do
+      it "returns an array" do
+        expect(FarMar::Market.all).to be_an Array
+      end
+    end
+
+    describe "self.find" do
+      # it "takes an integer as an argument" do
+      #   expect(FarMar::Market.find(num)).to be_an_instance of FarMar::Market
+      # end
+      it "returns an instance of market" do
+        num = rand(1..500)
+        expect(FarMar::Market.find(num)).to be_an_instance_of FarMar::Market
+      end
+      it "returns an instance of market with the correct ID" do
+        num = 1
+        market = FarMar::Market.find(num)
+        expect(market.id).to eq 1
+      end
+    end
+
+    describe "id" do
+      it "returns the ID of the market" do
+        @market = FarMar::Market.new({
+        :id => 123,
+        :name => "market name",
+        :address => "market address",
+        :city => "market city",
+        :county => "market county",
+        :state => "market state",
+        :zip => 12345
+        })
+        expect(@market.id).to eq 123
       end
     end
   end
