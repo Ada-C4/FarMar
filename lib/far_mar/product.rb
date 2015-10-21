ac336f93d6e3a9511d4ee55632ca3a30fcfc98f4
emgord <emgord@gmail.com>
2015-10-20 17:06:10 -0700

    added initialize and self.all for product. one test still failing

--- lib/far_mar/product.rb
+++ lib/far_mar/product.rb
@@ -1,6 +1,24 @@
 module FarMar
 
   class Product
+
+    attr_reader :id, :name, :vendor_id
+
+    def initialize(id, name, vendor_id)
+      @id = id.to_i
+      @name = name
+      @vendor_id = vendor_id.to_i
+    end
+
+    def self.all
+      product_array = CSV.read("./support/products.csv")
+
+      product_array.map! do |product|
+        FarMar::Product.new(product[0],product[1],product[2])
+      end
+        return product_array
+    end
+
   end
 
 end
--- spec/far_mar/product_spec.rb
+++ spec/far_mar/product_spec.rb
@@ -2,11 +2,39 @@ require "spec_helper"
 
 describe FarMar::Product do
   before :each do
-    @product = FarMar::Product.new
+    @product = FarMar::Product.new(1, "tomato", 2)
+    @all_products = FarMar::Product.all
   end
   describe ".new" do
     it "creates a new instance of product" do
       expect(@product).to be_an_instance_of FarMar::Product
     end
   end
+  describe "#id" do
+    it "returns unique id for the product" do
+      expect(@product.id).to eq 1
+    end
+  end
+  describe "#name" do
+    it "returns name for the product" do
+      expect(@product.name).to eq "tomato"
+    end
+  end
+  describe "#vendor_id" do
+    it "returns the vendor ID for the product" do
+      expect(@product.vendor_id).to eq 2
+    end
+  end
+  describe "self.all" do
+    it "creates an array" do
+      expect(@all_products).to be_an Array
+    end
+    it "elements are instances of FarMar::Product" do
+      expect(@all_products[0]).to be_an_instance_of FarMar::Product
+    end
+    it "creates a product for each row in the csv file" do
+      expect(@all_markets.length).to eq 8193
+    end
+  end
+
 end
