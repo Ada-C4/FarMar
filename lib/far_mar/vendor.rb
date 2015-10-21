bfd8ef5af71b040e322816d7bb58818976b23fdd
Amy Hunter <amyrhunter@gmail.com>
2015-10-20 18:57:47 -0700

    Added working self.find(id) method to Vendor class. Fixed self.all method in the Vendor class to return an array of instances instead of an array of arrays.

--- lib/far_mar/vendor.rb
+++ lib/far_mar/vendor.rb
@@ -2,7 +2,7 @@ module FarMar
   class Vendor
     attr_accessor :id, :name, :no_of_employees, :market_id
       def initialize(id, name, no_of_employees, market_id)
-        @id = id
+        @id = id.to_i
         @name = name
         @no_of_employees = no_of_employees
         @market_id = market_id
@@ -12,14 +12,16 @@ module FarMar
         #returns a collection of vendor instances, representing all of the vendors described in the CSV
         vendor_array = []
         CSV.read("./support/vendors.csv").each do |vendor|
-          print vendor
           list_of_vendors = Vendor.new(vendor[0], vendor[1], vendor[2], vendor[3])
           vendor_array.push(list_of_vendors)
         end
+        return vendor_array
       end
 
-      # def self.find(id)
-      # end
+      def self.find(id)
+        vendor_array = self.all
+        vendor_array.find {|i| i.id == id}
+      end
 
   end
 end
