6c60140ec24eeb6f7a1413921ed17a5cf1a40341
Jennie Buechner <janina.buechner@gmail.com>
2015-10-21 14:03:24 -0700

    refactor base find and all methods

--- lib/far_mar/far_mar_base.rb
+++ lib/far_mar/far_mar_base.rb
@@ -8,30 +8,33 @@ module FarMar
     end
 
     def self.all
-      csv_file = CSV.read(self::FILENAME)
-      # Create empty array which will hold all the objects
-      objects = []
-      csv_file.each do |row|
-        # Convert the array to a hash
-        # binding.pry
-        csv_hash = self.convert_to_hash(row)
-        # Create a product object from each row-hash in the csv file
-        temp = self.new(csv_hash)
-        # Push product object to array of products
-        objects.push(temp)
+      if !self.class_variable_defined?(:@@all_objects)
+        csv_file = CSV.read(self::FILENAME)
+        # Create empty array which will hold all the objects
+        objects = []
+        csv_file.each do |row|
+          # Convert the array to a hash
+          csv_hash = self.convert_to_hash(row)
+          # Create a product object from each row-hash in the csv file
+          temp = self.new(csv_hash)
+          # Push product object to array of products
+          objects.push(temp)
+        end
+        self.class_variable_set(:@@all_objects, objects)
       end
-      return objects
     end
 
     def self.find(id)
-      csv_file = CSV.read(self::FILENAME)
-      match_record = csv_file.find { |a| a[0].to_i == id}
-      if match_record == nil
-        return nil
-      else
-        hash = self.convert_to_hash(match_record)
-        return self.new(hash)
-      end
+      # binding.pry
+      # csv_file = CSV.read(self::FILENAME)
+      # match_record = csv_file.find { |a| a[0].to_i == id}
+      # if match_record == nil
+      #   return nil
+      # else
+      #   hash = self.convert_to_hash(match_record)
+      #   return self.new(hash)
+      # end
+      self.all_objects.find { |obj| obj.id == id }
     end
 
   end
