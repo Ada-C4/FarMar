70fd21b46b7bbbe931c1ea129c46a2ce47317ff8
Jennie Buechner <janina.buechner@gmail.com>
2015-10-21 10:34:20 -0700

    create vendor self.by_market method

--- lib/far_mar/vendor.rb
+++ lib/far_mar/vendor.rb
@@ -15,5 +15,16 @@ module FarMar
       vendor[:market_id] = array[3].to_i
       return vendor
     end
+
+    def self.by_market(market_id)
+      csv_file = CSV.read(FILENAME)
+      matches = csv_file.find_all { |row| row[3].to_i == market_id }
+      vendors = []
+      matches.each do |vendor_array|
+        vendor_hash = convert_to_hash(vendor_array)
+        vendors.push(FarMar::Vendor.new(vendor_hash))
+      end
+      return vendors
+    end
   end
 end
--- spec/far_mar/vendor_spec.rb
+++ spec/far_mar/vendor_spec.rb
@@ -13,6 +13,8 @@ describe FarMar do
         expect(vendor.emp_num).to eq(10)
         expect(vendor.market_id).to eq(2)
       end
+    end
+    describe "self.all" do
       it "returns an array of 2690 Vendor objects" do
         vendors = FarMar::Vendor.all
         expect(vendors).to be_an(Array)
@@ -20,18 +22,32 @@ describe FarMar do
         expect(vendors[0].id).to eq(1)
         expect(vendors[-1].id).to eq(2690)
       end
+    end
 
-      describe ".find(id)" do
-        it "returns Vendor object for matching ID" do
-          result = FarMar::Vendor.find(2690)
-          expect(result).to be_an_instance_of(FarMar::Vendor)
-          expect(result.id).to eq(2690)
-          expect(result.name).to eq("Mann-Lueilwitz")
-          expect(result.emp_num).to eq(4)
-          expect(result.market_id).to eq(500)
-        end
+    describe ".find(id)" do
+      it "returns Vendor object for matching ID" do
+        result = FarMar::Vendor.find(2690)
+        expect(result).to be_an_instance_of(FarMar::Vendor)
+        expect(result.id).to eq(2690)
+        expect(result.name).to eq("Mann-Lueilwitz")
+        expect(result.emp_num).to eq(4)
+        expect(result.market_id).to eq(500)
       end
+    end
 
+    describe ".by_market(market_id)" do
+      it "returns nil for an invalid market_id" do
+        expect(FarMar::Vendor.by_market('a')).to eq([])
+        expect(FarMar::Vendor.by_market([1, 2])).to eq([])
+        expect(FarMar::Vendor.by_market(-5)).to eq([])
+        expect(FarMar::Vendor.by_market(999999999999)).to eq([])
+      end
+      it "returns all the vendors with the given market_id" do
+        vendors = FarMar::Vendor.by_market(12)
+        expect(vendors).to be_an_instance_of(Array)
+        expect(vendors.length).to eq(3)
+        expect(vendors[0].id).to eq(51)
+      end
     end
   end
 end
