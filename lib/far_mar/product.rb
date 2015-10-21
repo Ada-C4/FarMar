8cdf1c67866de1a0312476a65689be4841f2e297
Amy Hunter <amyrhunter@gmail.com>
2015-10-20 18:46:20 -0700

    Added working self.find(id) method and corrected self.all method to return an array of instances instead of an array of arrays.

--- lib/far_mar/product.rb
+++ lib/far_mar/product.rb
@@ -2,7 +2,7 @@ module FarMar
   class Product
     attr_accessor :id, :name, :vendor_id
     def initialize(id, name, vendor_id)
-      @id = id
+      @id = id.to_i
       @name = name
       @vendor_id = vendor_id
     end
@@ -10,11 +10,18 @@ module FarMar
     def self.all
       #returns a collection of product instances, representing all of the products described in the CSV
       product_array = []
-      CSV.read("./support/products.csv").each do |product|
-        print product
-        list_of_products = Product.new(product[0], product[1], product[2])
+      product_file = CSV.read("./support/products.csv")
+      product_file.each do |product|
+        list_of_products = FarMar::Product.new(product[0], product[1], product[2])
         product_array.push(list_of_products)
       end
+      return product_array
+    end
+
+    def self.find(id)
+      #returns an instance of Product where the value in the id field of the CSV file matches the passed parameter
+      product_array = self.all
+      product_array.find {|i| i.id == id}
     end
   end
 end
