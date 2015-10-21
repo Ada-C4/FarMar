c13adb74172e6f5f52b84d2cdacebe9c30198046
Wesley Willis <haileyw@gmail.com>
2015-10-21 15:47:02 -0700

    add instance and class method for Sale class

--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -1,7 +1,25 @@
 module FarMar
   class Sale
-    def initialize
-
+    attr_accessor :id, :amount, :purchase_time, :vendor_id, :product_id
+    def initialize(sale_hash)
+      @id = sale_hash[:id].to_i
+      @amount = sale_hash[:amount].to_i
+      @purchase_time = DateTime.parse(sale_hash[:purchase_time])
+      @vendor_id = sale_hash[:vendor_id]
+      @product_id = sale_hash[:product_id]
     end
+
+    def self.all
+      @@sales_all ||= []
+      if @@sales_all == []
+        CSV.read("support/sales.csv").each do |id, amount, purchase_time, vendor_id, product_id|
+          sale_hash = {:id => id, :amount => amount, :purchase_time => purchase_time, :vendor_id => vendor_id, :product_id => product_id}
+          market = FarMar::Sale.new(sale_hash)
+          @@sales_all.push(market)
+        end
+      end
+      return @@sales_all
+    end
+
   end
 end
--- spec/far_mar/sale_spec.rb
+++ spec/far_mar/sale_spec.rb
@@ -3,12 +3,20 @@ require "spec_helper"
 describe FarMar do
   describe FarMar::Sale do
     before :each do
-      @sale = FarMar::Sale.new
+      #@sale = FarMar::Sale.new
     end
 
     describe "#initialize" do
       it "creates a new instance of Sale" do
-        expect(@sale).to be_an_instance_of FarMar::Sale
+        fake_hash = {:id => 6, :amount => 150, :purchase_time => "2007-01-31 12:22:26", :vendor_id => 555, :product_id => 4}
+        test = FarMar::Sale.new(fake_hash)
+        expect(test).to be_an_instance_of FarMar::Sale
+      end
+    end
+
+    describe '#self.all' do
+      it "returns a collection of Sale instances" do
+        expect(FarMar::Sale.all.length).to eq CSV.read("support/sales.csv").length
       end
     end
   end
