643cc61fe671fdc0469822b9e1264638fd2dc044
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-10-21 11:20:26 -0700

    Added all and find methods for vendor class and specs for those methods

--- lib/far_mar/vendor.rb
+++ lib/far_mar/vendor.rb
@@ -1,4 +1,47 @@
 module FarMar
   class Vendor
+    attr_accessor :id, :name, :address, :market_id
+      def initialize(vendor_hash)
+        @id = vendor_hash[:id]
+        @name = vendor_hash[:name]
+        @address = vendor_hash[:address]
+        @market_id = vendor_hash[:market_id]
+      end
+
+      def self.all()
+        vendors_csv = CSV.read("./support/vendors.csv")
+        vendors_array = []
+        vendors_csv.each do |line|
+        new_vendor = FarMar::Vendor.new(
+        {
+          :id => line[0].to_i,
+          :name => line[1],
+          :address => line[2],
+          :market_id => line[3].to_i
+          }
+        )
+        vendors_array.push(new_vendor)
+      end
+      return vendors_array
+      end
+
+      def self.find(id)
+        vendors_csv = CSV.read("./support/vendors.csv")
+        match = vendors_csv.find {|vendor| vendor[0].to_i == id}
+        new_vendor = FarMar::Vendor.new(
+        {
+          :id => match[0].to_i,
+          :name => match[1],
+          :address => match[2],
+          :market_id => match[3].to_i
+          }
+        )
+        return new_vendor
+      end
+
+
+      def id
+        return @id
+      end
   end
 end
--- spec/far_mar/vendor_spec.rb
+++ spec/far_mar/vendor_spec.rb
@@ -4,7 +4,31 @@ describe FarMar do
   describe FarMar::Vendor do
     describe "initialize" do
       it "creates an instance of a Vendor" do
-        expect(FarMar::Vendor.new).to be_an_instance_of FarMar::Vendor
+        @vendor_hash = {
+        :id => 123,
+        :name => "vendor name",
+        :num_employees => "vendor address",
+        :market_id => 456
+        }
+        expect(FarMar::Vendor.new(@vendor_hash)).to be_an_instance_of FarMar::Vendor
+      end
+    end
+
+    describe "self.all" do
+      it "returns an array" do
+        expect(FarMar::Vendor.all).to be_an Array
+      end
+    end
+
+    describe "self.find" do
+      it "returns an instance of vendor" do
+        num = rand(1..2690)
+        expect(FarMar::Vendor.find(num)).to be_an_instance_of FarMar::Vendor
+      end
+      it "returns an instance of vendor with the correct ID" do
+        num = 1
+        vendor = FarMar::Vendor.find(num)
+        expect(vendor.id).to eq 1
       end
     end
   end
