038c1cf7a74a791963b9d3d40360ea4c54a96cbd
emgord <emgord@gmail.com>
2015-10-21 10:35:00 -0700

    added initialize and self.all method for sale

--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -1,6 +1,26 @@
 module FarMar
 
   class Sale
+
+    attr_reader :id, :amount, :purchase_time, :vendor_id, :product_id
+
+    def initialize(id, amount, purchase_time, vendor_id, product_id)
+      @id = id.to_i
+      @amount = amount.to_i
+      @purchase_time = DateTime.parse(purchase_time)
+      @vendor_id = vendor_id.to_i
+      @product_id = product_id.to_i
+    end
+
+    def self.all
+      sale_array = CSV.read("./support/sales.csv")
+
+      sale_array.map! do |sale|
+        FarMar::Sale.new(sale[0],sale[1],sale[2],sale[3],sale[4])
+      end
+        return sale_array
+    end
+
   end
 
 end
--- spec/far_mar/sale_spec.rb
+++ spec/far_mar/sale_spec.rb
@@ -2,11 +2,48 @@ require "spec_helper"
 
 describe FarMar::Sale do
   before :each do
-    @sale = FarMar::Sale.new
+    @sale = FarMar::Sale.new("1", "120", "2013-11-11 11:29:52 -0800", "2", "123")
+    @all_sales = FarMar::Sale.all
   end
   describe ".new" do
     it "creates a new instance of sale" do
       expect(@sale).to be_an_instance_of FarMar::Sale
+    end
+  end
+  describe "#id" do
+    it "returns unique id for the sale" do
+      expect(@sale.id).to eq 1
+    end
+  end
+  describe "#amount" do
+    it "returns the amount for the sale" do
+      expect(@sale.amount).to eq 120
+    end
+  end
+  describe "#purchase_time" do
+    it "returns the purchase time for the sale that is a Datetime" do
+      expect(@sale.purchase_time).to be_a(DateTime)
+    end
+  end
+  describe "#vendor_id" do
+    it "returns the vendor id for the sale" do
+      expect(@sale.vendor_id).to eq 2
+    end
+  end
+  describe "#product_id" do
+    it "returns the product id for the sale" do
+      expect(@sale.product_id).to eq 123
+    end
+  end
+  describe "self.all" do
+    it "creates an array" do
+      expect(@all_sales).to be_an Array
+    end
+    it "elements are instances of FarMar::Sale" do
+      expect(@all_sales[0]).to be_an_instance_of FarMar::Sale
+    end
+    it "creates a product for each row in the csv file" do
+      expect(@all_sales.length).to eq 12798
     end
   end
 end
