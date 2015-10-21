3f6fe49ff37a2bbc4b9337ed1f8cdaee4d245d38
Lauren Granger <lr.granger21@gmail.com>
2015-10-21 11:30:45 -0700

    so much partygit add spec/far_mar/markets_spec.rb  self.find worksgit add spec/far_mar/markets_spec.rb

--- lib/far_mar/market_class.rb
+++ lib/far_mar/market_class.rb
@@ -1,9 +1,9 @@
+require "pry"
+
 module FarMar
   class Market
 
-    attr_accessor :market_array, :all_markets_array
-
-    @@num_of_markets = 0
+    attr_accessor :id, :name, :address, :city, :county, :state, :zip
 
   # Each individual market has many vendors associated with it. The `FarMar::Market` data, in order in the CSV, consists of:
   #
@@ -15,36 +15,46 @@ module FarMar
   # 6. State - (String) state in which the market is located
   # 7. Zip - (String) zipcode in which the market is located
 
-    def initialize
-      @market_array = CSV.read("./support/markets.csv")
-      @id = @market_array[@@num_of_markets][0]
-      @name = @market_array[@@num_of_markets][1]
-      @address = @market_array[@@num_of_markets][2]
-      @city = @market_array[@@num_of_markets][3]
-      @county = @market_array[@@num_of_markets][4]
-      @state = @market_array[@@num_of_markets][5]
-      @zip = @market_array[@@num_of_markets][6]
-      @@num_of_markets += 1
+    def initialize(id, name, address, city, county, state, zip)
+      @id = id
+      @name = name
+      @address = address
+      @city = city
+      @county = county
+      @state = state
+      @zip = zip
     end
 
 # - `self.all` - returns a collection of Market instances, representing all of the markets described in the CSV
     def self.all
       market_array = CSV.read("./support/markets.csv")
-      @all_markets_array = []
-      @@num_of_markets = 0
-      while @@num_of_markets < (market_array.length) do
-        @all_markets_array.push(FarMar::Market.new)
-      end
-      return @all_markets_array
+      all_markets_array = []
+      num_of_markets = 0
+        market_array.each do |single_array|
+          id = single_array[0]
+          name = single_array[1]
+          address = single_array[2]
+          city = single_array[3]
+          county = single_array[4]
+          state = single_array[5]
+          zip = single_array[6]
+        all_markets_array.push(FarMar::Market.new(id, name, address, city, county, state, zip))
+        end
+      return all_markets_array
     end
-#
+
 # # - `self.find(id)` - returns an instance of Market where the value of the `id` field in the CSV matches the passed parameter.
-#
-#     def self.find(id)
-#       @all_markets_array.find do |x|
-#         x = id
-#       end
-#     end
+
+    def self.find_market(market_id)
+      all_the_markets = FarMar::Market.all
+      all_the_markets.find do |single_market|
+        if single_market.id == market_id.to_s
+        var = []
+        var = ["#{single_market.id}", "#{single_market.name}", "#{single_market.address}", "#{single_market.city}", "#{single_market.county}", "#{single_market.state}", "#{single_market.zip}"]
+        return var
+      end
+      end
+    end
 
 # - `vendors` - returns a collection of `FarMar::Vendor` instances that are associated with the market by the `market_id` field.
 
--- spec/far_mar/markets_spec.rb
+++ spec/far_mar/markets_spec.rb
@@ -4,18 +4,34 @@ require "./lib/far_mar/market_class.rb"
 describe FarMar::Market do
 
   describe "initialize"
-  before :each do
-    @market_test = FarMar::Market.new
-  end
     it "creates a new instace of the market class" do
+      @market_test = FarMar::Market.new(0, "name", "1234 street", "city", "county", "state", 12345)
       expect(@market_test).to be_an_instance_of FarMar::Market
     end
     it "converts the csv file to an array" do
-      expect(@market_test.market_array).to be_an Array
+      @market_test = FarMar::Market.new(0, "name", "1234 street", "city", "county", "state", 12345)
+      expect(FarMar::Market.all).to be_an Array
     end
 
     describe "self.all"
-      it "returns an array of all markets in the csv file" do
-        expect(FarMar::Market.all.length).to eq 500
+      it "returns an array" do
+        expect(FarMar::Market.all).to be_an Array
       end
+      it "returns all markets in the csv file" do
+        expect(FarMar::Market.all.count).to eq 500
+      end
+
+    describe "self.find"
+    it "returns the information for a given market ID" do
+      expect(FarMar::Market.find_market(303)).to eq ["303", "Madison County Farmers Market", "1022 Cook Avenue", "Huntsville", "Madison", "Alabama", "35804"]
+      end
+
+#     describe "vendors"
+#     FarMar::Vendor.new
+#       it "returns a collection" do
+#         expect(@market_test.vendors(1)).to be_an Array
+#       end
+#       it "returns all vendors for a given market id" do
+#         expect(@market_test.vendors(1)).to eq #and array with all of the market 1 vendors
+#       end
 end
