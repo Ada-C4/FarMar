99d7d0b271414d1a60c251a0f907c90d8a590df5
Amy Hunter <amyrhunter@gmail.com>
2015-10-21 14:47:54 -0700

    Added working self.find(id) rspec with two tests.

--- spec/far_mar/vendor_spec.rb
+++ spec/far_mar/vendor_spec.rb
@@ -3,11 +3,13 @@ require "spec_helper"
 describe FarMar::Vendor do
   before :each do
     @vendor = FarMar::Vendor.new(45,"Hyatt, Conroy and Ortiz",5,10)
+    @vendor_2 = FarMar::Vendor.new(2690, "Mann-Lueilwitz", 4, 500)
   end
 
   context "#initializing" do
     it "creates an instance of @vendor" do
       expect(@vendor).to be_an_instance_of FarMar::Vendor
+      expect(@vendor_2).to be_an_instance_of FarMar::Vendor
     end
   end
 
@@ -19,4 +21,17 @@ describe FarMar::Vendor do
       expect(FarMar::Vendor.all().length).to eq 2690
     end
   end
+
+  context ".self.find(id)" do
+    it "returns an instance of Vendor" do
+      expect(@vendor.class).to be FarMar::Vendor
+    end
+
+    it "returns an instance with the same id as the value in the CSV file" do
+      expect(@vendor.id).to eq 45
+      expect(@vendor_2.id).to eq 2690
+    end
+  end
+
+
 end
