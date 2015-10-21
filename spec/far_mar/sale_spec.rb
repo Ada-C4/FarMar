f4b08a530f054a3d0d1964b73dc71abfe6d3186d
Amy Hunter <amyrhunter@gmail.com>
2015-10-21 15:00:07 -0700

    Added working self.find(id) method to sale_spec.rb. All tests working.

--- spec/far_mar/sale_spec.rb
+++ spec/far_mar/sale_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 describe FarMar::Sale do
   before :each do
     @sale = FarMar::Sale.new(14, 4978, "2013-11-10 01:51:24 -0800", 3, 4)
+    @sale_2 = FarMar::Sale.new(12001, 8923.0, "2013-11-12 02:03:31 -0800", 2690, 8192)
   end
 
   context "initializing" do
@@ -17,6 +18,17 @@ describe FarMar::Sale do
     end
     it "returns instances of all lines in the csv" do
       expect(FarMar::Sale.all().length).to eq 12798
+    end
+  end
+
+  context ".self.find(id)" do
+    it "returns an instance of Sale" do
+      expect(@sale.class).to be FarMar::Sale
+    end
+
+    it "returns an instance with the same id as the value in the CSV file" do
+      expect(@sale.id).to eq 14
+      expect(@sale_2.id).to eq 12001
     end
   end
 end
