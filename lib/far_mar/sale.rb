01f4cab5bd40bdf8a10989266723635e1ac6b6b8
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-10-21 13:44:48 -0700

    Added sale all and find methods and specs for both methods

--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -1,4 +1,50 @@
 module FarMar
   class Sale
+    attr_accessor :id, :amount, :purchase_time, :vendor_id, :product_id
+      def initialize(sale_hash)
+        @id = sale_hash[:id]
+        @amount = sale_hash[:amount]
+        @purchase_time = sale_hash[:purchase_time]
+        @vendor_id = sale_hash[:vendor_id]
+        @product_id = sale_hash[:product_id]
+      end
+
+      def self.all()
+        sales_csv = CSV.read("./support/sales.csv")
+        sales_array = []
+        sales_csv.each do |line|
+        new_sale = FarMar::Sale.new(
+        {
+          :id => line[0].to_i,
+          :amount => line[1],
+          :purchase_time => line[2],
+          :vendor_id => line[3].to_i,
+          :product_id => line[4].to_i
+          }
+        )
+        sales_array.push(new_sale)
+      end
+      return sales_array
+      end
+
+      def self.find(id)
+        sales_csv = CSV.read("./support/sales.csv")
+        match = sales_csv.find {|sale| sale[0].to_i == id}
+        new_sale = FarMar::Sale.new(
+        {
+          :id => match[0].to_i,
+          :amount => match[1],
+          :purchase_time => match[2],
+          :vendor_id => match[3].to_i,
+          :product_id => match[4].to_i
+          }
+        )
+        return new_sale
+      end
+
+
+      def id
+        return @id
+      end
   end
 end
--- spec/far_mar/sale_spec.rb
+++ spec/far_mar/sale_spec.rb
@@ -4,7 +4,32 @@ describe FarMar do
   describe FarMar::Sale do
     describe "initialize" do
       it "creates an instance of a Sale" do
-        expect(FarMar::Sale.new).to be_an_instance_of FarMar::Sale
+        @sale_hash = {
+        :id => 123,
+        :amount => 150,
+        :purchase_time => "2013-11-07 14:41:55 -0800",
+        :vendor_id => 456,
+        :product_id => 123
+        }
+        expect(FarMar::Sale.new(@sale_hash)).to be_an_instance_of FarMar::Sale
+      end
+    end
+
+    describe "self.all" do
+      it "returns an array" do
+        expect(FarMar::Sale.all).to be_an Array
+      end
+    end
+
+    describe "self.find" do
+      it "returns an instance of sale" do
+        num = rand(1..12001)
+        expect(FarMar::Sale.find(num)).to be_an_instance_of FarMar::Sale
+      end
+      it "returns an instance of sale with the correct ID" do
+        num = 1
+        sale = FarMar::Sale.find(num)
+        expect(sale.id).to eq 1
       end
     end
   end
