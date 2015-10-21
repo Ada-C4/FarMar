29dd84b858071a273c6cbd1d21dc78b69e2dcf67
emgord <emgord@gmail.com>
2015-10-21 10:48:08 -0700

    added initialize and self.all method for vendors

--- lib/far_mar/vendor.rb
+++ lib/far_mar/vendor.rb
@@ -1,6 +1,25 @@
 module FarMar
 
   class Vendor
+
+    attr_reader :id, :name, :employees, :market_id
+
+    def initialize(id, name, employees, market_id)
+      @id = id.to_i
+      @name = name
+      @employees = employees.to_i
+      @market_id = market_id.to_i
+    end
+
+    def self.all
+      vendor_array = CSV.read("./support/vendors.csv")
+
+      vendor_array.map! do |vendor|
+        FarMar::Vendor.new(vendor[0],vendor[1],vendor[2],vendor[3])
+      end
+        return vendor_array
+    end
+
   end
 
 end
--- spec/far_mar/vendor_spec.rb
+++ spec/far_mar/vendor_spec.rb
@@ -2,11 +2,43 @@ require "spec_helper"
 
 describe FarMar::Vendor do
   before :each do
-    @vendor = FarMar::Vendor.new
+    @vendor = FarMar::Vendor.new("1","Frank's Farm" , "4", "1")
+    @all_vendors = FarMar::Vendor.all
   end
   describe ".new" do
     it "creates a new instance of vendor" do
       expect(@vendor).to be_an_instance_of FarMar::Vendor
+    end
+  end
+  describe "#id" do
+    it "returns unique id for the vendor" do
+      expect(@vendor.id).to eq 1
+    end
+  end
+  describe "#name" do
+    it "returns the name for the vendor" do
+      expect(@vendor.name).to eq "Frank's Farm"
+    end
+  end
+  describe "#employees" do
+    it "returns the number of employees for the vendor" do
+      expect(@vendor.employees).to eq 4
+    end
+  end
+  describe "#market_id" do
+    it "returns the market id for the vendor" do
+      expect(@vendor.market_id).to eq 1
+    end
+  end
+  describe "self.all" do
+    it "creates an array" do
+      expect(@all_vendors).to be_an Array
+    end
+    it "elements are instances of FarMar::Vendor" do
+      expect(@all_vendors[0]).to be_an_instance_of FarMar::Vendor
+    end
+    it "creates a vendor for each row in the csv file" do
+      expect(@all_vendors.length).to eq 2690
     end
   end
 end
