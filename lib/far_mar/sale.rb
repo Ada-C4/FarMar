93b063664e428fbd4e8111aa2ff08846f2471fd7
Amy Hunter <amyrhunter@gmail.com>
2015-10-20 18:53:26 -0700

    Added working self.find(id) method to the Sale class. Fixed self.all method in Sale class to return an array of instances instead of an array of arrays.

--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -2,7 +2,7 @@ module FarMar
   class Sale
     attr_accessor :id, :amount, :purchase_time, :vendor_id, :product_id
     def initialize(id, amount, purchase_time, vendor_id, product_id)
-      @id = id
+      @id = id.to_i
       @amount = amount
       @purchase_time = purchase_time
       @vendor_id = vendor_id
@@ -13,10 +13,15 @@ module FarMar
       #returns a collection of sale instances, representing all of the sales described in the CSV
       sale_array = []
       CSV.read("./support/sales.csv").each do |sale|
-        print sale
         list_of_sales = Sale.new(sale[0], sale[1], sale[2], sale[3], sale[4])
         sale_array.push(list_of_sales)
       end
+      return sale_array
+    end
+
+    def self.find(id)
+      sale_array = self.all
+      sale_array.find {|i| i.id == id}
     end
   end
 end
