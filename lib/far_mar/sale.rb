a535010c61e1c6b8600dd447997471f84b276a07
Desiree <desireepoland@gmail.com>
2015-10-21 11:17:27 -0700

    Update Sale #initialize to accept a hash

--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -1,6 +1,14 @@
 module FarMar
   class Sale
 
+    def initialize(sale_hash)
+      @id = sale_hash[:id].to_i
+      @amount = sale_hash[:amount].to_i
+      @purchase_time = DateTime.parse(sale_hash[:purchase_time])
+      @vendor_id = sale_hash[:vendor_id].to_i
+      @product_id = sale_hash[:product_id].to_i
+    end
+
     def self.all
     end
 
--- spec/far_mar/sale_spec.rb
+++ spec/far_mar/sale_spec.rb
@@ -2,7 +2,13 @@ require "spec_helper"
 
 describe FarMar::Sale do
   before :each do
-    @sale = FarMar::Sale.new
+    @sale = FarMar::Sale.new({
+      id: "1",
+      amount: "9290",
+      purchase_time: "2013-11-07 04:34:56 -0800",
+      vendor_id: "1",
+      product_id:"1"
+      })
   end
 
   describe ".new" do
