afad36ca1e266b6da820c2db2d100b605c2a01e3
Daphne <dg1181@nyu.edu>
2015-10-23 21:02:00 -0700

    Added randomized spec thingy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,7 @@ FarMar::Market::CSV_FILE = "./support/sample_markets.csv"
 FarMar::Vendor::CSV_FILE = "./support/sample_vendors.csv"
 FarMar::Product::CSV_FILE = "./support/sample_products.csv"
 FarMar::Sale::CSV_FILE = "./support/sample_sales.csv"
+
+RSpec.configure do |config|
+ config.order = 'random'
+end
